a8674045d5817b3ea35421228e655bac6c2cab0e
Student <kotulska.martyna1@gmail.com>
2024-10-12 12:12:01 +0200

    abc

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args){
-            System.out.println("Hello world");
+        int a = 5;
+        int b = 6;
+        int c = a + b;
+        System.out.println(a+" + "+b+" = "+c);
     }
 }
