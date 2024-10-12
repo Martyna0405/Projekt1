cdd2c6f2b742b0a886a5bb90c5184b808746e9b4
Student <kotulska.martyna1@gmail.com>
2024-10-12 12:54:15 +0200

    abc

--- Main.java
+++ Main.java
@@ -4,5 +4,22 @@ public class Main {
         int b = 6;
         int c = a + b;
         System.out.println(a+" + "+b+" = "+c);
+        System.out.println(++c);
+        System.out.println(--c);
+
+        boolean d=true;
+        boolean h=false;
+        boolean g=false;
+        System.out.println(d && h);
+        System.out.println(d || h);
+        System.out.println(g || h);
+
+        int aa = 4;
+        if (aa>7){
+            System.out.println("Liczba jest wieksza od 7 i jest nią: "+aa);
+        }
+        else{
+            System.out.println("Liczba jest mniejsza od 7");
+        }
     }
 }
